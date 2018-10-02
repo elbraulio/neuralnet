1237082aa0cd00a946dc9ba27b8c6725c2c0d33b
elbraulio <brauliop.3@gmail.com>
2018-10-01 23:31:23 -0300

    more test

--- src/test/java/com/elbraulio/neuralnet/DefaultBiasOperationTest.java
+++ src/test/java/com/elbraulio/neuralnet/DefaultBiasOperationTest.java
@@ -16,6 +16,10 @@ public class DefaultBiasOperationTest {
                 new DefaultBiasOperation(3, -1.5).intValue(),
                 is(1)
         );
+        assertThat(
+                new DefaultBiasOperation(-3, -1.5).intValue(),
+                is(0)
+        );
     }
 
     @Test
@@ -23,6 +27,10 @@ public class DefaultBiasOperationTest {
         assertThat(
                 new DefaultBiasOperation(3L, -1.5).longValue(),
                 is(1L)
+        );
+        assertThat(
+                new DefaultBiasOperation(-3L, -1.5).longValue(),
+                is(0L)
         );
     }
 
@@ -32,6 +40,10 @@ public class DefaultBiasOperationTest {
                 new DefaultBiasOperation(3f, -1.5f).floatValue(),
                 is(1f)
         );
+        assertThat(
+                new DefaultBiasOperation(-3f, -1.5f).floatValue(),
+                is(0f)
+        );
     }
 
     @Test
@@ -39,6 +51,14 @@ public class DefaultBiasOperationTest {
         assertThat(
                 new DefaultBiasOperation(3.3d, -3.31d).doubleValue(),
                 is(0d)
+        );
+        assertThat(
+                new DefaultBiasOperation(3.4d, -3.3d).doubleValue(),
+                is(0d)
+        );
+        assertThat(
+                new DefaultBiasOperation(4d, -3.3d).doubleValue(),
+                is(1d)
         );
     }
 }
