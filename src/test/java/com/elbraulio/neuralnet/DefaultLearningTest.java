0ff8ae24ab8d4f933bf4b7785d7709a18b36d558
elbraulio <brauliop.3@gmail.com>
2018-10-08 15:02:28 -0300

    Learning introduced

---
+++ src/test/java/com/elbraulio/neuralnet/DefaultLearningTest.java
@@ -0,0 +1,29 @@
+package com.elbraulio.neuralnet;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Braulio Lopez (brauliop.3@gmail.com)
+ */
+public class DefaultLearningTest {
+    @Test
+    public void noLearningRate(){
+        NeuralArgs newArgs = new DefaultLearning(0).newArgs(
+                0, new DefaultArgs(1d,1,1),1,1
+        );
+        assertThat(newArgs.bias(), is(1d));
+        assertThat(newArgs.weights(), is(new Number[]{1d,1d}));
+    }
+
+    @Test
+    public void learnFromCorrectOnes(){
+        NeuralArgs newArgs = new DefaultLearning(1d).newArgs(
+                1, new DefaultArgs(1d,1d,1d),1,1
+        );
+        assertThat(newArgs.bias(), is(1d));
+        assertThat(newArgs.weights(), is(new Number[]{1d,1d}));
+    }
+}
