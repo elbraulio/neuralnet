a688960bfe46451d77637ea15898dcc6af616644
elbraulio <brauliop.3@gmail.com>
2018-11-05 02:21:28 -0300

    neural network working

---
+++ src/main/java/com/elbraulio/neuralnet/SaveToTimeline.java
@@ -0,0 +1,27 @@
+package com.elbraulio.neuralnet;
+
+/**
+ * @author Braulio Lopez (brauliop.3@gmail.com)
+ */
+public final class SaveToTimeline implements NeuralUnit {
+    private final NeuralUnit unit;
+    private final int layer;
+    private final int index;
+    private final TimeLine timeLine;
+
+    public SaveToTimeline(
+            NeuralUnit unit, int layer, int index, TimeLine timeLine
+    ) {
+        this.unit = unit;
+        this.layer = layer;
+        this.index = index;
+        this.timeLine = timeLine;
+    }
+
+    @Override
+    public Number feed(Number... inputs) {
+        final Number output = this.unit.feed(inputs);
+        this.timeLine.saveOutput(output, this.layer, this.index);
+        return output;
+    }
+}
